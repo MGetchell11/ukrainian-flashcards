8ee3ee070c80c240db6c16320e39ffad6bb73c49
Matt <MGetchell11@yahoo.com>
2020-11-23 15:49:56 -0500

    fixed English translation in main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 
 import random
 from wordDict import words
-from sys import exit
-
-ukrainian_word = random.choice(list(words.keys()))
 
 def flash_cards():
     instructions = "INSTRUCTIONS: Type z for random Ukrainian word, x for English translation, q to quit"
     print(instructions + "\n")
-    x = ukrainian_word
-    print(x + "\n")
-    user_input = input("Type z, x, or q\n")
-    if user_input == "z":
-        print(random.choice(list(words.keys())))
-    elif user_input == "x":
-        print(dict(words.values(x)))
-    elif user_input == "q":
-        exit(0)
-    else:
-        print("Not a valid input, please try again.")
+    while True:
+        user_input = input("Type z, x, or q\n")
+        if user_input == "z":
+            z = random.choice(list(words.keys()))
+            print(z + "\n")
+        elif user_input == "x":
+            x = words.get(z)
+            print(z + " = " + x + "\n")
+            return flash_cards()
+        elif user_input == "q":
+            break
+        else:
+            print("Not a valid input, please try again.\n")
+            return flash_cards()
 flash_cards()
